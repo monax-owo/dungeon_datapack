42c82b20dcba14de880f84085627b10a76542561
monax-owo <zprl.dev@gmail.com>
2026-01-18 20:59:19 +0900

    up py,gen

--- gen_loot_table.py
+++ gen_loot_table.py
@@ -61,7 +61,7 @@ def gen_lt(id, tier: int, price: int, name: str, lore: object) -> Optional[dict[
     if name is None or name == "":
         print("name is None")
     else:
-        data["pools"][0]["entries"][0]["functions"][0]["components"]["minecraft:custom_name"] = name
+        data["pools"][0]["entries"][0]["functions"][0]["components"]["minecraft:custom_name"] = {"text": name, "italic": False}
 
     if lore is None or lore == "":
         print("lore is None")
