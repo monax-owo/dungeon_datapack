63a2d408f480bdddcd92777edf9211f691bcd552
monax-owo <zprl.dev@gmail.com>
2026-03-17 14:23:51 +0900

    up

--- script/gen.ts
+++ script/gen.ts
@@ -3,5 +3,5 @@ import { gen_equipment_loot_tables } from "./equipments";
 
 (async function () {
   await gen_item_loot_tables();
-  await gen_equipment_loot_tables();
+  // await gen_equipment_loot_tables();
 })();
